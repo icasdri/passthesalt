7f71e065b9dfe7e52947404be24f8ce75810112e
icasdri <icasdri@gmail.com>
2016-07-14 01:19:22 -0400

    Another attempt to statically build and link libsodium

--- sodium/get_libsodium.sh
+++ sodium/get_libsodium.sh
@@ -8,23 +8,23 @@ set -e
 [ -f libsodium.asc ]
 
 VERSION='1.0.10'
-TARGET="libsodium-${VERSION}"
-TARGET_FILE="${TARGET}.tar.gz"
-SIG_FILE="${TARGET_FILE}.sig"
+SOURCE="libsodium-${VERSION}"
+SOURCE_FILE="${SOURCE}.tar.gz"
+SIG_FILE="${SOURCE_FILE}.sig"
 
-wget "https://download.libsodium.org/libsodium/releases/${TARGET_FILE}"
+wget "https://download.libsodium.org/libsodium/releases/${SOURCE_FILE}"
 wget "https://download.libsodium.org/libsodium/releases/${SIG_FILE}"
 
 gpg --import libsodium.asc
 gpg --verify "$SIG_FILE"
 
-tar -zxvf "$TARGET_FILE"
+tar -zxvf "$SOURCE_FILE"
 
-cd "$TARGET"
+cd "$SOURCE"
 
 ./configure
 make
 make check
+make DESTDIR=../dest install
 
 cd ..
-mv "$TARGET" libsodium_lib
