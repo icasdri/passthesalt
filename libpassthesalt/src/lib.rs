a21bc21dbebcd554fc485ad0b0d198d6af8fac3a
icasdri <icasdri@gmail.com>
2016-07-15 22:36:08 -0400

    Skeleton for overall interaction with argument passing, main error handling, and calling libpassthesalt

--- libpassthesalt/src/lib.rs
+++ libpassthesalt/src/lib.rs
@@ -1,3 +1,20 @@
+extern crate sodiumoxide as sodium;
+
+use sodium::crypto::box_;
+
+pub enum PtsError {
+    Fatal(&'static str)
+}
+use PtsError as PE;
+
+pub fn init() -> Result<(), PtsError> {
+    if sodium::init() {
+        Ok(())
+    } else {
+        Err(PE::Fatal("Failed to initialize libsodium encryption facilities"))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,75 @@
 /*
  * Copyright 2016 icasdri
- * 
- * This file is part of passthesalt. 
+ *
+ * This file is part of passthesalt.
  * See COPYING for licensing details.
  */
 
+use std::io::Write;
+
+extern crate libpassthesalt as pts;
+use pts::PtsError as PE;
+
 #[macro_use]
 extern crate clap;
+use clap::ArgMatches;
 
-use clap::{Arg, App, SubCommand, AppSettings};
+mod args;
+
+enum MainError<'a> {
+    UsageProblem(&'a ArgMatches<'a>, &'static str),
+    InvalidInput,
+    Inner(PE)
+}
+use MainError as ME;
+
+fn handle_key<'a>(m: &'a ArgMatches) -> Result<(), MainError<'a>> {
+    try!(pts::init().map_err(|e| MainError::Inner(e)));
+
+    if m.is_present("new_key") {
+        Ok(())
+    } else {
+        Err(ME::UsageProblem(m, concat!(
+              "error: Please specify a flag to perform a key-related operation.\n",
+              "       For instance, pass --new to generate a new key pair.")))
+    }
+}
+
+fn handle_encrypt(m: &ArgMatches) {
+    // pts::init();
+}
+
+fn handle_decrypt(m: &ArgMatches) {
+    // pts::init();
+}
 
 fn main() {
-    let priv_key_arg =
-        Arg::with_name("priv_key")
-            .short("i")
-            .long("identity")
-            .long("me")
-            .value_name("keyfile")
-            .takes_value(true)
-            .help("Your private key file");
+    let mut stderr = std::io::stderr();
+    let matches = args::app().get_matches();
 
-    let pub_key_arg =
-        Arg::with_name("pub_key")
-            .short("t")
-            .long("them")
-            .value_name("key")
-            .takes_value(true)
-            .help("Their public key (directly, from a file, or as a contact name)");
+    let result = match matches.subcommand() {
+        ("key", Some(m)) => handle_key(m),
+        // ("encrypt", Some(m)) => handle_encrypt(m),
+        // ("decrypt", Some(m)) => handle_decrypt(m),
+        _ => unreachable!()
+    };
 
-    let contacts_file_arg =
-        Arg::with_name("contacts_file")
-            .short("c")
-            .long("contacts")
-            .value_name("file")
-            .takes_value(true)
-            .help("The contacts file to look up contacts from");
-
-    let input_file_arg =
-        Arg::with_name("input_file")
-            .value_name("FILE")
-            .takes_value(true)
-            .help("A file to encrypt/decrypt (leave off to read from stdin)");
-
-    let key_subcommand =
-        SubCommand::with_name("key")
-            .arg(Arg::with_name("new_key")
-                 .short("n")
-                 .long("new")
-                 .help("Generate new public/private key pair"))
-            .about("Generate new keys and other key-related operations");
-
-    let encrypt_subcommand =
-        SubCommand::with_name("encrypt")
-            .arg(priv_key_arg.clone().required(true))
-            .arg(pub_key_arg.clone().required(true))
-            .arg(input_file_arg.clone())
-            // .arg(contacts_file_arg.clone())
-            .about("Encrypt a message to a recipient's public key");
-
-    let decrypt_subcommand =
-        SubCommand::with_name("decrypt")
-            .arg(priv_key_arg.clone().required(true))
-            .arg(pub_key_arg.clone().required(true))
-            .arg(input_file_arg.clone())
-            // .arg(contacts_file_arg.clone())
-            .about("Decrypt a message from a sender's public key");
-
-    let app =
-        App::new("passthesalt")
-            .version(crate_version!())
-            .global_setting(AppSettings::VersionlessSubcommands)
-            .global_setting(AppSettings::SubcommandRequired)
-            .subcommand(key_subcommand.display_order(1))
-            .subcommand(encrypt_subcommand.display_order(2))
-            .subcommand(decrypt_subcommand.display_order(3));
-
-    let matches = app.get_matches();
-
+    if let Err(err) = result {
+        let exit_code = match err {
+            ME::UsageProblem(u, message) => {
+                writeln!(
+                    stderr,
+                    "{}\n\n{}\n\nFor more information try --help",
+                    message,
+                    u.usage(),
+                );
+                1
+            },
+            ME::Inner(PE::Fatal(message)) => {
+                writeln!(stderr, "{}", message);
+                102
+            }
+            _ => unimplemented!()
+        };
+    }
 }
